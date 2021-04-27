1212e2f86fd770afc3b0a81b3177fadacffe0d80
Alex Sanders <a@snd.rs>
2021-04-27 10:00:10 +0100

    sort exports

--- packages/source/src/index.ts
+++ packages/source/src/index.ts
@@ -1,72 +1,64 @@
 export {
-	visuallyHidden,
-	focusHalo,
 	descriptionId,
+	focusHalo,
 	generateSourceId,
+	visuallyHidden,
 } from './foundations/accessibility';
-
 export { transitions } from './foundations/animation';
-
 export { breakpoints } from './foundations/breakpoints';
 export type { Breakpoint } from './foundations/breakpoints';
-
-export { from, until, between } from './foundations/media-queries';
+export { between, from, until } from './foundations/media-queries';
 export type { BreakpointMap } from './foundations/media-queries';
-
 export {
 	brand,
 	brandAlt,
-	neutral,
+	culture,
 	error,
-	success,
+	labs,
+	lifestyle,
+	neutral,
 	news,
 	opinion,
-	sport,
-	culture,
-	lifestyle,
-	labs,
 	specialReport,
+	sport,
+	success,
 } from './foundations/palette';
-
-export { defaultTheme, brand, brandAlt } from './foundations/themes';
-export { accordionDefault } from './foundations/themes/accordion';
 export {
-	buttonDefault,
-	buttonBrand,
-	buttonBrandAlt,
-} from './foundations/themes/button';
-export type { ButtonTheme } from './foundations/themes/button';
-export { checkboxDefault, checkboxBrand } from './foundations/themes/checkbox';
-export { choiceCardDefault } from './foundations/themes//choice-card';
-export { footerBrand } from './foundations/themes/footer';
-export { labelDefault, labelBrand } from './foundations/themes/label';
-export {
-	linkDefault,
-	linkBrand,
-	linkBrandAlt,
-} from './foundations/themes/link';
-export type { LinkTheme } from './foundations/themes/link';
-export { radioDefault, radioBrand } from './foundations/themes/radio';
-export { selectDefault } from './foundations/themes/select';
-export { textInputDefault } from './foundations/themes/text-input';
-export {
-	userFeedbackDefault,
-	userFeedbackBrand,
-} from './foundations/themes/user-feedback';
-
-export {
+	background,
 	brandAltBackground,
 	brandBackground,
-	background,
 } from './foundations/palette/background';
 export {
+	border,
 	brandAltBorder,
 	brandBorder,
-	border,
 } from './foundations/palette/border';
 export { brandAltLine, brandLine, line } from './foundations/palette/line';
 export { brandAltText, brandText, text } from './foundations/palette/text';
-
-export { height, remHeight, width, remWidth } from './foundations/size';
-
-export { space, remSpace } from './foundations/space';
+export { height, remHeight, remWidth, width } from './foundations/size';
+export { remSpace, space } from './foundations/space';
+export { brand, brandAlt, defaultTheme } from './foundations/themes';
+export { choiceCardDefault } from './foundations/themes//choice-card';
+export { accordionDefault } from './foundations/themes/accordion';
+export {
+	buttonBrand,
+	buttonBrandAlt,
+	buttonDefault,
+} from './foundations/themes/button';
+export type { ButtonTheme } from './foundations/themes/button';
+export { checkboxBrand, checkboxDefault } from './foundations/themes/checkbox';
+export { footerBrand } from './foundations/themes/footer';
+export { labelBrand, labelDefault } from './foundations/themes/label';
+export {
+	linkBrand,
+	linkBrandAlt,
+	linkDefault,
+} from './foundations/themes/link';
+export type { LinkTheme } from './foundations/themes/link';
+export { radioBrand, radioDefault } from './foundations/themes/radio';
+export { selectDefault } from './foundations/themes/select';
+export { textInputDefault } from './foundations/themes/text-input';
+export {
+	userFeedbackBrand,
+	userFeedbackDefault,
+} from './foundations/themes/user-feedback';
