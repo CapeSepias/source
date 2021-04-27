8a2388897f1105cb4e7669cdd9c7354c730a40c5
Alex Sanders <a@snd.rs>
2021-04-27 10:08:23 +0100

    move icons

--- packages/source/src/index.ts
+++ packages/source/src/index.ts
@@ -1,14 +1,28 @@
+// ############################# accessibility #############################
+
 export {
 	descriptionId,
 	focusHalo,
 	generateSourceId,
 	visuallyHidden,
 } from './foundations/accessibility';
+
+// ############################# animation #############################
+
 export { transitions } from './foundations/animation';
+
+// ############################# breakpoints #############################
+
 export { breakpoints } from './foundations/breakpoints';
 export type { Breakpoint } from './foundations/breakpoints';
+
+// ############################# media queries #############################
+
 export { between, from, until } from './foundations/media-queries';
 export type { BreakpointMap } from './foundations/media-queries';
+
+// ############################# palette #############################
+
 export {
 	brand,
 	brandAlt,
@@ -35,8 +49,17 @@ export {
 } from './foundations/palette/border';
 export { brandAltLine, brandLine, line } from './foundations/palette/line';
 export { brandAltText, brandText, text } from './foundations/palette/text';
+
+// ############################# size #############################
+
 export { height, remHeight, remWidth, width } from './foundations/size';
+
+// ############################# space #############################
+
 export { remSpace, space } from './foundations/space';
+
+// ############################# themes #############################
+
 export { brand, brandAlt, defaultTheme } from './foundations/themes';
 export { choiceCardDefault } from './foundations/themes//choice-card';
 export { accordionDefault } from './foundations/themes/accordion';
@@ -62,3 +85,48 @@ export {
 	userFeedbackBrand,
 	userFeedbackDefault,
 } from './foundations/themes/user-feedback';
+
+// ############################# icons #############################
+
+export { SvgAlert } from './icons/alert';
+export { SvgAlertRound } from './icons/alert-round';
+export { SvgAlertTriangle } from './icons/alert-triangle';
+export { SvgArrowDownStraight } from './icons/arrow-down-straight';
+export { SvgArrowLeftStraight } from './icons/arrow-left-straight';
+export { SvgArrowRightStraight } from './icons/arrow-right-straight';
+export { SvgArrowUpStraight } from './icons/arrow-up-straight';
+export { SvgAudio } from './icons/audio';
+export { SvgCamera } from './icons/camera';
+export { SvgCheckmark } from './icons/checkmark';
+export { SvgChevronDownSingle } from './icons/chevron-down-single';
+export { SvgChevronLeftSingle } from './icons/chevron-left-single';
+export { SvgChevronRightSingle } from './icons/chevron-right-single';
+export { SvgChevronUpSingle } from './icons/chevron-up-single';
+export { SvgCreditCard } from './icons/credit-card';
+export { SvgCross } from './icons/cross';
+export { SvgDirectDebit } from './icons/direct-debit';
+export { SvgDirectDebitWide } from './icons/direct-debit-wide';
+export { SvgEnvelope } from './icons/envelope';
+export { SvgExternal } from './icons/external';
+export { SvgEye } from './icons/eye';
+export { SvgEyeStrike } from './icons/eye-strike';
+export { SvgFacebook } from './icons/facebook';
+export { SvgHouse } from './icons/house';
+export { SvgIndent } from './icons/indent';
+export { SvgInfo } from './icons/info';
+export { SvgLinkedIn } from './icons/linked-in';
+export { SvgMessenger } from './icons/messenger';
+export { SvgMinus } from './icons/minus';
+export { SvgPerson } from './icons/person';
+export { SvgPayPal } from './icons/paypal';
+export { SvgPlay } from './icons/play';
+export { SvgPlus } from './icons/plus';
+export { SvgPinterest } from './icons/pinterest';
+export { SvgQuote } from './icons/quote';
+export { SvgSettings } from './icons/settings';
+export { SvgSpeechBubble } from './icons/speech-bubble';
+export { SvgStar } from './icons/star';
+export { SvgTickRound } from './icons/tick-round';
+export { SvgTwitter } from './icons/twitter';
+export { SvgVideo } from './icons/video';
+export { SvgWhatsApp } from './icons/whats-app';
